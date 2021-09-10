1a4def62bf82c754f3e809bddd3197dfb5dd844e
JuanseIG <89666312+JuanseIG@users.noreply.github.com>
2021-09-10 17:06:52 -0300

    Repuesto

---
+++ VentaRespuestos.BibliotecaClases/Categoria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaRespuestos.BibliotecaClases
+{
+    public class Categoria
+    {
+        private int _codigo;
+        private string _nombre; 
+
+        public Categoria (int codigo, string nombre)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+        }
+
+    }
+}
---
+++ VentaRespuestos.BibliotecaClases/Repuesto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentaRespuestos.BibliotecaClases
+{
+    class Repuesto
+    {
+        private int _codigo;
+        private string _nombre;
+        private double _precio;
+        private int _stock;
+        private Categoria _categoria;
+
+        public Repuesto(int codigo, string nombre, double precio, int stock)
+        {
+            _codigo = codigo;
+            _nombre = nombre;
+            _precio = precio;
+            _stock = stock;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+        }
+
+
+
+    }
+}
